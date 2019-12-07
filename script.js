043a3a21c4715f4c49bfc938304cb0f4bbf02132
Dustin Bates <dpbates13@gmail.com>
2019-12-07 13:50:29 -0600

    working

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const idList = [];
-const bearer = "Bearer BQAz_Uducobg1M1k8T34WDa1sXFHC_9uQQbCLshRZa1yXa0b80ltQiuVw0vi_nn-v-v5qnWUc-KeSBf8s7tRZw3xmIe_5jXEysTY7b9TCHnDIzYMkCmlIrGl43CWOyZPdMv_qOR5lAFIjGN9";
+const url = "https://api.spotify.com/v1/search?q=tag%3Anew&type=album&limit=50";
+const bearer = "Bearer BQB_okUu2kNmALlF_khaYVMy4Meog4IecpW755oEXk88CvYFYie0nfYh4OlYUWvP0-6XuCcGduH3Mf9y_Im0XUdJPm0Y3Ff21B-LwHDnqpNtvo3hShjKXLNq1K5bxq7iQmyJTTflbGArZw";
+let idList = [];
+let idArray = [];
 
 function getIds(url) {
     fetch(url, {
@@ -13,25 +15,53 @@ function getIds(url) {
         return response.json();
         //console.log(response.json());
     }).then(responseJson => {
+        //let idList = [];
         for (let i=0; i<responseJson.albums.items.length; i++) {
             idList.push(responseJson.albums.items[i].id);
         }
         //console.log(database);
         if (responseJson.albums.next !== null) {
-            getIds(responseJson.albums.next);
+            doIt(responseJson.albums.next);
+        }
+        //return idList;
+    }).then(response => {
+        createIdArray(idList);
+        console.log(idList);
+    });
+}
+
+function doIt(next) {
+    fetch(next, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': bearer
+        }
+    }).then(response => {
+        return response.json();
+        //console.log(response.json());
+    }).then(responseJson => {
+        //let idList = [];
+        for (let i=0; i<responseJson.albums.items.length; i++) {
+            idList.push(responseJson.albums.items[i].id);
+        }
+        //console.log(database);
+        if (responseJson.albums.next !== null) {
+            doIt(responseJson.albums.next);
         }
     })
 }
 
 function createIdArray(idList) {
-    let idArray = [];
     let count = 0;
     let idString = "";
-    for (i = 0; i<idList.length; i++) {
+    for (i = 0; i<10000; i++) {
         if (count == 19) {
             idString += `${idList[i]}`;
             idArray.push(idString);
             count = 0;
+            idString = "";
         }
         else {
             idString += `${idList[i]},`;
@@ -42,14 +72,12 @@ function createIdArray(idList) {
 
 }
 
-function initialDatabaseConstruction() {
-    $('form').submit(event => {
-        event.preventDefault();
-        console.log('working');
-        getIds("https://api.spotify.com/v1/search?q=tag%3Anew&type=album&limit=50");
-        console.log(idList);
-        createIdArray(idList);
-    })
+function createAlbumDatabase(idArray) {
+    console.log(idArray);
 }
 
-$(initialDatabaseConstruction);
+function createInitialDatabase() {
+    getIds(url);
+}
+
+$(createInitialDatabase);
